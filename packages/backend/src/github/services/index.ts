f27dfb265580223b02acf678b8f1e7894c0cab99
Brandon Ly <brandonly@lostcoding.com>
2025-05-23 12:32:28 -0500

    Feat/GitHub api (#16)

---
+++ packages/backend/src/github/services/index.ts
@@ -0,0 +1,31 @@
+import { getOctokitClient } from "../utils/github-api";
+
+export async function getPullRequestRatio(): Promise<number> {
+	const client = getOctokitClient();
+
+	const pullRequests = await client.pulls.list({
+		owner: "biomejs",
+		repo: "biome",
+		state: "open",
+	});
+
+	let reviewedPRCount = 0;
+
+	await Promise.all(
+		pullRequests.data.map(async (pullRequest) => {
+			// look at pull request review comments to see if PR has been reviewed
+			// use the review_comments_url to get the review comments on a PR
+			// if there are no review comments i.e. it hasn't been reviewed yet, then we know it hasn't been reviewed
+			// if the array is NOT empty, we know it's been reviewed
+			pullRequest.review_comments_url;
+			const reviewComments = await client.pulls.listReviewComments({
+				owner: "biomejs",
+				repo: "biome",
+				pull_number: pullRequest.number,
+			});
+			reviewedPRCount += reviewComments.data.length ? 1 : 0;
+		}),
+	);
+
+	return reviewedPRCount / pullRequests.data.length;
+}
